3611138042622b95c126eff13fb5877f555301cc
KBR <kbr@ucl.dk>
2021-11-22 11:27:31 +0100

    Concurrency added

--- Booking.Domain/Model/BookingCalendar.cs
+++ Booking.Domain/Model/BookingCalendar.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Booking.Domain.Model
 {
     public class BookingCalendar
     {
         public Guid Id { get; set; }
+        public string Name { get; set; }
+  
+        public byte[] Concurrency { get; set; }   
         public List<Booking> Bookings { get; set; } = new();
     }
 }
